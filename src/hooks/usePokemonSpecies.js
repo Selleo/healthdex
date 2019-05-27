c231fdb5b1a8aed85ef329dffd6dffc880f20887
Skysplit <t.romik@selleo.com>
2019-05-27 16:43:33 +0200

    Remove unused utilities

--- src/hooks/usePokemonSpecies.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { PokemonSpeciesContext } from '../contexts/PokemonSpeciesContext';
-
-export function usePokemonSpecies() {
-  return useContext(PokemonSpeciesContext);
-}
