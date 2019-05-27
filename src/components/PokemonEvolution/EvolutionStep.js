34a15c01aa50989d75c3ba117f6c7398fef6e65c
Skysplit <t.romik@selleo.com>
2019-05-27 19:18:16 +0200

    Skip evolution requirements

--- src/components/PokemonEvolution/EvolutionStep.js
+++ src/components/PokemonEvolution/EvolutionStep.js
@@ -6,14 +6,10 @@ export const EvolutionStep = memo(function EvolutionStep(props) {
   const { evolution } = props;
   const { evolution_details, evolves_to, species: { name } } = evolution;
   const isFirstInChain = isEmpty(evolution_details)
-  const style = isFirstInChain ? {} : { paddingLeft: 20, borderLeft: '1px solid grey' }
+  const style = { paddingLeft: 20, borderLeft: '1px solid grey', borderTop: '1px solid grey' };
 
   return (
     <div style={style}>
-      <hr />
-      {evolution_details.map((detail, index) => (
-        <span key={index}>detail {index}</span>
-      ))}
       {isFirstInChain || <>{' '}&raquo;{' '}</>}
       <PokemonShortInfoContainer pokemonName={name} />
       {evolves_to.map((nextEvolution) => (
