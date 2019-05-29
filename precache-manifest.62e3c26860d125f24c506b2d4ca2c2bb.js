72a315209168b801a7db81c349580d0eee939c26
Skysplit <t.romik@selleo.com>
2019-05-29 15:49:10 +0200

    Updates

---
+++ precache-manifest.62e3c26860d125f24c506b2d4ca2c2bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3da262fde7fa3ee35d50fa83f9b16f",
+    "url": "/healthdex/index.html"
+  },
+  {
+    "revision": "2d506694880de2c7f409",
+    "url": "/healthdex/static/css/main.5c9a680b.chunk.css"
+  },
+  {
+    "revision": "430a649f2e969fa5ecaa",
+    "url": "/healthdex/static/js/2.a7a4d727.chunk.js"
+  },
+  {
+    "revision": "2d506694880de2c7f409",
+    "url": "/healthdex/static/js/main.82ff0fb9.chunk.js"
+  },
+  {
+    "revision": "e02277e8adf10bd71383",
+    "url": "/healthdex/static/js/runtime~main.32e27f26.js"
+  }
+]);
